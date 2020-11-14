75d08ae45b37eaeba10b9f687f97502a8ecbadca
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-14 16:50:19 +0530

    folder and file style is changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
-import { Counter } from "./features/counter/Counter";
+import React from "react"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
+      <h1>Lets build iMessage clone</h1>
     </div>
   );
 }
