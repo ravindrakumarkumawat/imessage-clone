42ff5c3624a7d9cc058527720db3896c7233f430
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-15 19:29:15 +0530

    signin fuctionality is added through auth and provider of firebase

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,19 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import { auth, provider } from '../../firebase'
 import './Login.css'
 
 const Login = () => {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error)=> alert(error.message))
+  }
   return (
     <div className="login">
       <div className="login__logo">
         <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1605447194/imessage/IMessage_logo_szhoj4.png" alt="imessage-logo"/>
         <h1>iMessage</h1>
       </div>
-      <Button>Sign In</Button>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   )
 }
