055ae7fbb185e0db91c2adc68a5d677a8f0a008e
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-15 19:59:15 +0530

    Logout functionality is added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,22 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { RateReviewOutlined, Search } from '@material-ui/icons'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import SidebarChat from '../SidebarChat/SidebarChat'
 import './Sidebar.css'
+import { selectUser } from '../../features/userSlice'
+import { auth } from '../../firebase'
 
 const Sidebar = () => {
+  const user = useSelector(selectUser)
+
   return (
     <div className="sidebar">
       <div className='sidebar__header'>
-        <Avatar className='sidebar__avatar'/>
+        <Avatar 
+          src={user.photo} 
+          onClick={() => auth.signOut()}
+          className='sidebar__avatar' />
         <div className='sidebar__input'>
           <Search />
           <input placeholder='Search' />
