156e51c6596a94f00b0a34385c0d5f80247ab8b9
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-15 19:50:24 +0530

    login functionality is added through google sigin and we are fetching user data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react"
-import { useSelector } from 'react-redux'
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux'
 import "./App.css"
 import Imessage from './components/Imessage/Imessage'
 import Login from "./components/Login/Login"
-import {selectUser} from './features/userSlice'
+import { selectUser, login, logout } from './features/userSlice'
+import { auth } from './firebase'
 
 function App() {
   const user = useSelector(selectUser)
+
+  const dispatch = useDispatch()
+
+  useEffect (() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // User is logged in
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName
+        }))
+      } else {
+        // User is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
+
   return (
     <div className="app">
       { user ? <Imessage /> : <Login /> }
