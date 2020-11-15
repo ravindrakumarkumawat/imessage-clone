c8835694d3ebdeb9cbcc2e9141a360f06e241c66
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-15 16:22:01 +0530

    working on the input field added some style there

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import './Chat.css'
+import { IconButton } from "@material-ui/core";
+import { MicNone } from "@material-ui/icons";
+import React, { useState } from "react";
+import "./Chat.css";
 
 const Chat = () => {
+  const [input, setInput] = useState("");
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    // Firebase Handling...
+    setInput("")
+  };
   return (
     <div className="chat">
-    { /* Chat header */ }
-    <div className="chat__header">
-      <h4>To: <span className="chat__name">Channel Name</span></h4>
-      <strong>Details</strong>
+      <div className="chat__header">
+        <h4>
+          To: <span className="chat__name">Channel Name</span>
+        </h4>
+        <strong>Details</strong>
+      </div>
+      {/* chat messages */}
+      {/* Chat input */}
+      <div className="chat__input">
+        <form>
+          <input 
+            value={input} 
+            onChange={(e) => setInput(e.target.value)} 
+            type="text" 
+            placeholder="iMessage" />
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
+        <IconButton>
+          <MicNone />
+        </IconButton>        
+      </div>
     </div>
-    { /* chat messages */ }
-    { /* Chat input */ }
-    </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
