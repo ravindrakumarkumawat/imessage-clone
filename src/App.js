b9054c269820c644bb12f9a30266b5314f41ca4f
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-15 18:32:55 +0530

    app rendering based user login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react"
+import { useSelector } from 'react-redux'
 import "./App.css"
 import Imessage from './components/Imessage/Imessage'
+import {selectUser} from './features/userSlice'
 
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="app">
-      <Imessage />
+      { user ? <Imessage /> : <h2>You need to login</h2> }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
