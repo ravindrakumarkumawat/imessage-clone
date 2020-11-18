c6a70db03610583cbdb409227c8d13df392c5066
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-18 18:04:08 +0530

    timeago is added

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux'
 import { setChat } from '../../features/chatSlice'
 import db from '../../firebase'
 import './SidebarChat.css'
+import * as timeago from 'timeago.js'
 
 const SidebarChat = ({id, chatName}) => {
   const dispatch = useDispatch()
@@ -31,7 +32,7 @@ const SidebarChat = ({id, chatName}) => {
         <h3>{chatName}</h3>
         <p>{chatInfo[0]?.message}</p>
         <small>{
-          new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()
+          timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString())
         }</small>
       </div>
     </div>
