9202a1d30aa8792be0d5de0678650a8ab2efc9de
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-18 16:28:37 +0530

    data is passing to the dom

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,13 +1,29 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../features/userSlice'
 import './Message.css'
 
-const Message = ({id, contents}) => {
+const Message = ({
+  id, 
+  contents: {
+    timestamp,
+    message,
+    uid,
+    photo,
+    email,
+    displayName
+  }
+}) => {
+  const user = useSelector(selectUser)
   return (
-    <div className="message">
-      <Avatar />
-      <p>This is a message</p>
-      <small>timestamp</small>
+    <div className={`message ${user.email === email && 'message__sender'}`}>
+      <Avatar 
+        src={photo}
+        className="message__photo"
+        />
+      <p>{message}</p>
+      <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
     </div>
   )
 }
