4b9c97553a9c218d53ad8b4a5812a0d20e73475b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-18 14:36:14 +0530

    Chat is selected and stored

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -1,10 +1,23 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setChat } from '../../features/chatSlice'
 import './SidebarChat.css'
 
 const SidebarChat = ({id, chatName}) => {
+  const dispatch = useDispatch()
+  
   return (
-    <div className="sidebarChat">
+    <div
+      onClick={() => {
+        dispatch(
+          setChat({
+            chatId: id,
+            chatName: chatName
+          })
+        )
+      }}
+      className="sidebarChat">
       <Avatar />
       <div className="sidebarChat__info">
         <h3>{chatName}</h3>
