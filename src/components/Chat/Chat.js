1f6e053fceeee50d7d243bf49aea0ea07732b296
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-18 14:41:03 +0530

    chat is selected and displayed

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,14 @@
 import { IconButton } from "@material-ui/core";
 import { MicNone } from "@material-ui/icons";
 import React, { useState } from "react";
+import { useSelector } from 'react-redux';
+import { selectChatName } from '../../features/chatSlice'
 import Message from "../Message/Message";
 import "./Chat.css";
 
 const Chat = () => {
   const [input, setInput] = useState("");
+  const chatName = useSelector(selectChatName)
   const sendMessage = (e) => {
     e.preventDefault();
 
@@ -16,7 +19,7 @@ const Chat = () => {
     <div className="chat">
       <div className="chat__header">
         <h4>
-          To: <span className="chat__name">Channel Name</span>
+          To: <span className="chat__name">{chatName}</span>
         </h4>
         <strong>Details</strong>
       </div>
